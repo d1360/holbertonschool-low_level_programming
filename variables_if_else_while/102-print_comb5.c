1e9b316e7fa03f5ebc99fbaf6d21d624f5e5178e
d1360 <diegoalt@gmail.com>
2024-08-12 18:25:25 +0000

    102

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -2,27 +2,27 @@
 
 int main(void)
 {
-    int i, j; /* Declare variables outside of the for loop */
+	int i, j; /* Declare variables outside of the for loop */
 
-    /* Loop through all combinations of two-digit numbers */
-    for (i = 0; i <= 99; i++)
-    {
-        for (j = i + 1; j <= 99; j++)
-        {
-            putchar((i / 10) + '0');
-            putchar((i % 10) + '0');
-            putchar(' ');
-            putchar((j / 10) + '0');
-            putchar((j % 10) + '0');
+	/* Loop through all combinations of two-digit numbers */
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 
-            /* Print comma and space unless it's the last combination */
-            if (i != 98 || j != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n'); /* Print new line at the end */
-    return (0);
+			/* Print comma and space unless it's the last combination */
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n'); /* Print new line at the end */
+	return (0);
 }
