ccf73fd0be75930c4bd8e5c8d4645725dae62ad9
d1360 <diegoalt@gmail.com>
2024-08-12 16:51:57 +0000

    9-print

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Description: This program prints all single-digit numbers from 0 to 9,
+ *              each followed by a comma and space, except for the last number.
+ *              The program uses only the putchar function and avoids using 
+ *              any char variables.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
