3493e0f4142a7fff240aa62f6d663434f376ceef
d1360 <diegoalt@gmail.com>
2024-08-12 22:41:40 +0000

    6.

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -3,10 +3,11 @@
 /**
  * _abs - Computes the absolute value of an integer.
  * @n: The integer to compute the absolute value of.
+ * 
  * Return: The absolute value of the integer.
  */
 int _abs(int n)
 {
-return (n < 0) ? -n : (n);
+    return (n < 0) ? -n : n;
 }
 
