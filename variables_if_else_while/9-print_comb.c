ec89ed05fa38f7179a85373b1d15834d1bd7147f
d1360 <diegoalt@gmail.com>
2024-08-12 16:53:59 +0000

    9-print

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,18 +12,18 @@
  */
 int main(void)
 {
-	int num;
+    int num;
 
-	for (num = 0; num <= 9; num++)
-	{
-		putchar(num + '0');
-		if (num != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
+    for (num = 0; num <= 9; num++)
+    {
+        putchar(num + '0');
+        if (num != 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
