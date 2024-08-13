81f8969701e55d6ea96ccd84be2a28ec0c80417a
d1360 <diegoalt@gmail.com>
2024-08-13 18:23:29 +0000

    104

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,17 +2,33 @@
 
 int main(void)
 {
-    unsigned long a = 1, b = 2, c;
-    int i;
+    unsigned long a_high = 0, a_low = 1;
+    unsigned long b_high = 0, b_low = 2;
+    unsigned long c_high, c_low;
+    unsigned long max = 1000000000;
 
-    printf("%lu, %lu", a, b);
+    printf("%lu, %lu", a_low, b_low);
 
-    for (i = 2; i < 98; i++)
+    for (int i = 2; i < 98; i++)
     {
-        c = a + b;
-        printf(", %lu", c);
-        a = b;
-        b = c;
+        c_low = a_low + b_low;
+        c_high = a_high + b_high;
+
+        if (c_low >= max) {
+            c_high += 1;
+            c_low -= max;
+        }
+
+        printf(", ");
+        if (c_high > 0)
+            printf("%lu%09lu", c_high, c_low);
+        else
+            printf("%lu", c_low);
+
+        a_low = b_low;
+        a_high = b_high;
+        b_low = c_low;
+        b_high = c_high;
     }
 
     printf("\n");
