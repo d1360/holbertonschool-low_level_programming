38ef03f34df699da0f2cdd7344b4a58dafb46ccf
d1360 <diegoalt@gmail.com>
2024-08-13 18:30:36 +0000

    104

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,37 +2,37 @@
 
 int main(void)
 {
-    unsigned long a_high = 0, a_low = 1;
-    unsigned long b_high = 0, b_low = 2;
-    unsigned long c_high, c_low;
-    unsigned long max = 1000000000;
-    int i;
+unsigned long a_high = 0, a_low = 1;
+unsigned long b_high = 0, b_low = 2;
+unsigned long c_high, c_low;
+unsigned long max = 1000000000;
+int i;
 
-    printf("%lu, %lu", a_low, b_low);
+printf("%lu, %lu", a_low, b_low);
 
-    for (i = 2; i < 98; i++)
-    {
-        c_low = a_low + b_low;
-        c_high = a_high + b_high;
+for (i = 2; i < 98; i++)
+{
+c_low = a_low + b_low;
+c_high = a_high + b_high;
 
-        if (c_low >= max) {
-            c_high += 1;
-            c_low -= max;
-        }
+if (c_low >= max) {
+c_high += 1;
+c_low -= max;
+}
 
-        printf(", ");
-        if (c_high > 0)
-            printf("%lu%09lu", c_high, c_low);
-        else
-            printf("%lu", c_low);
+printf(", ");
+if (c_high > 0)
+printf("%lu%09lu", c_high, c_low);
+else
+printf("%lu", c_low);
 
-        a_low = b_low;
-        a_high = b_high;
-        b_low = c_low;
-        b_high = c_high;
-    }
+a_low = b_low;
+a_high = b_high;
+b_low = c_low;
+b_high = c_high;
+}
 
-    printf("\n");
-    return (0);
+printf("\n");
+return (0);
 }
 
